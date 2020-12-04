e20cf8e850294b268db1cb533e44e9ede46b3681
UmayrKA <75446332+UmayrKA@users.noreply.github.com>
2020-12-04 17:57:53 -0500

    Add files via upload
    This is phase 2 of my swimmy game.

--- swimmy.py
+++ swimmy.py
@@ -16,9 +16,9 @@ class Enemy():
         # Shrink the enemy pic
         self.pic = pygame.transform.scale(self.pic, (int (self.size*1.25),self.size))
 
-        #Flip the pic the if the Enemy is moveing left
+        #Flip the pic if the Enemy is moving left
         if self.speed < 0:
-            self.pic=pygame.transform.flip(self.pic, True, False)
+            self.pic = pygame.transform.flip(self.pic, True, False)
 
     # Enemy uptade function (stuff to happen over and over again)       
     def update(self, screen):
@@ -39,7 +39,13 @@ running = True
 
 # Load all the pictures for our game
 background_pic = pygame.image.load("../assets/Scene_A.png")
+background_pic2 = pygame.image.load("../assets/Scene_B.png")
 player_pic = pygame.image.load("../assets/NewFish05_B.png")
+
+# Make some variables for the backgound animation
+bg_animation_timer_max = int(7.5)
+bg_animation_timer = bg_animation_timer_max
+bg_animation_frame = 0
 
 # Make some variables for our player
 player_starting_x = 480
@@ -68,7 +74,7 @@ title_y = play_button_y - 130
 
 
 # Make the enemy spawning timer variable
-enemy_timer_max = 40
+enemy_timer_max = 45
 enemy_timer = enemy_timer_max
 
 #Make the enemies array
@@ -99,14 +105,38 @@ while running:
         player_y -= player_speed
     if keys[pygame.K_SPACE]:
         player_size +=2
+
+    # Stop the player from leaving the screen
+    if player_x < 0:
+        player_x = 0
+    if player_x > game_width - player_size*1.25:
+        player_x = game_width - player_size*1.25
+    if  player_y < 0:
+        player_y = 0
+    if player_y > game_height - player_size:
+        player_y = game_height - player_size
+    
         
 
-    screen.blit(background_pic, (0,0))
+    # Stop the player from leaving the screen.
+        
+    # Do the background animation_timer
+    bg_animation_timer -= 1
+    if bg_animation_timer<= 0:
+        bg_animation_frame += 1
+        if bg_animation_frame > 1 :
+            bg_animation_frame = 0
+        bg_animation_timer = bg_animation_timer_max
+    
+    if bg_animation_frame == 0:
+        screen.blit(background_pic, (0,0))
+    else:
+        screen.blit(background_pic2, (0,0))
 
     # Spawn a new Enemy whenever enemy_timer hits 0
     enemy_timer -=1
     if enemy_timer <= 0:
-        new_enemy_y = random.randint(0, game_height-30)
+        new_enemy_y = random.randint(0, game_height - 30)
         new_enemy_speed = random.randint(1, 5)
         new_enemy_size = random.randint(player_size/2, player_size*2)
         if random.randint(0, 1) == 0:
@@ -123,6 +153,9 @@ while running:
     # Update all the enemies
     for enemy in enemies:
         enemy.update(screen)
+        if enemy.x < - 1000 or enemy.x > game_width+1000:
+            enemies_to_remove.append(enemy)
+        
     if player_alive:        
         # Update the player hitbox       
         player_hitbox.x = player_x
@@ -152,7 +185,7 @@ while running:
         score_text = score_font.render("Score: "+str(score), 1, (255,255,255))
     else:
         score_text = score_font.render("Final Score: "+str(score),1, (255,255,255))
-        if score >= 0:
+    if score >= 0:
             screen.blit(score_text, (30,30))
 
     # Draw the menu(when the player is not alive)
@@ -165,7 +198,7 @@ while running:
             if mouse_x > play_button_x and mouse_x < play_button_x+play_button_pic.get_width():
                 if mouse_y > play_button_y and mouse_y < play_button_y+play_button_pic.get_height():
                     # Restart the game
-                    player_alive =True
+                    player_alive = True
                     score = 0
                     player_x = player_starting_x
                     player_y= player_starting_y
